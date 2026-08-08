38585292b5e886efac8e5772999c114967ce42bf
Gokulnath K <k.gokulnath26@gmail.com>
2026-08-08 10:21:15 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from dashboard.db import run_query
+from db import run_query
 from charts.executive_charts import (
     competition_category_chart,
     player_country_chart,
